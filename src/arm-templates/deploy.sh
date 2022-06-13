0f5edbb3f44af285ae27846eaee47a2a88bfe342
dalrajphotoshoot1@gmail.com <sarabjeet.singh04@gmail.com>
2022-06-13 09:27:59 -0400

    Updated the pipeline

--- src/arm-templates/deploy.sh
+++ src/arm-templates/deploy.sh
@@ -1,10 +1,10 @@
 # Deploy the Rotator
-az deployment group create --name RotatorFunction --resource-group <rg> --template-file function.json  --parameters keyVaultName=<keyVaultName> functionAppName=<functionAppName> secretName=<secretName>
+az deployment group create --name RotatorFunction --resource-group 'testsrelabrg' --template-file function.json  --parameters keyVaultName='testsrelabkeyvault' functionAppName='testsrelabfapp' secretName='testsrelabsecret'
 
 # deploy the function app code
-func azure functionapp publish <functionAppName>
+func azure functionapp publish testsrelabfapp
 
 # Deploy the Event listener for the secret you want to rotate (one per secret, or simply remove the filter to include all events)
-az deployment group create --name RotatorEventSubscription --resource-group <rg> --template-file events.json  --parameters keyVaultName=<keyVaultName> functionAppName=<functionAppName> secretName=<secretName>
+az deployment group create --name RotatorEventSubscription --resource-group 'testsrelabrg' --template-file events.json  --parameters keyVaultName='testsrelabkeyvault' functionAppName='testsrelabfapp' secretName='testsrelabsecret'
 
 
